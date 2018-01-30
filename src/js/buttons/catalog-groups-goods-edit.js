c334851e1431f2730cc2249637b1947c61b40ec8
Dmitriy Kononov <kondor_06@mail.ru>
2018-01-30 15:34:23 +0300

    30.01

--- src/js/buttons/catalog-groups-goods-edit.js
+++ src/js/buttons/catalog-groups-goods-edit.js
@@ -3,27 +3,56 @@ import markup from './../markup/tools.js';
 import catalogGroupsGoods from './catalog-groups-goods.js';
 import formTools from './../tools/form-tools.js';
 
-const appUrl = window.appSettings.formAddGoods.UrlApi;
-const messages = window.appSettings.formAddGoods.message;
+const appUrl1 = window.appSettings.formEditGoods.UrlApi1;
+const appUrl2 = window.appSettings.formEditGoods.UrlApi2;
+const messages = window.appSettings.formEditGoods.message;
 
-const form = document.querySelector('#group-goods-add-form');
+const form = document.querySelector('#goods-card-form');
 
-const name = form.querySelector('#group-goods-name');
-const describe = form.querySelector('#group-goods-describe');
-// const priceGroup = form.querySelector('#group-goods-price');
-const purchase = form.querySelector('#group-goods-price-purchase');
-// const extra = form.querySelector('#group-goods-price-extra');
-const sell = form.querySelector('#group-goods-price-sell');
-const barcode = form.querySelector('#group-goods-barcode');
+const name = form.querySelector('#goods-card-name');
+const describe = form.querySelector('#goods-card-describe');
+const groupId = form.querySelector('#goods-card-group');
+const purchase = form.querySelector('#goods-card-price-purchase');
+// const precent = form.querySelector('#goods-card-price-total span');
+const sell = form.querySelector('#goods-card-price-sell');
+const barcode = form.querySelector('#goods-card-barcode');
 
 const callbackXhrSuccess = (response) => {
+  console.log('callbackXhr1');
+  console.dir(response);
 
   formTools.reset();
-  // $('#group-goods-add').modal('hide');
+  $('#goods-card').modal('hide');
 
   switch (response.status) {
   case 200:
-    catalogGroupsGoods.redraw();
+    catalogGroupsGoods.fill();
+    break;
+  case 400:
+    markup.informationtModal = {
+      'title': 'Error',
+      'messages': messages.mes400
+    };
+    break;
+  case 271:
+    markup.informationtModal = {
+      'title': 'Error',
+      'messages': response.messages
+    };
+    break;
+  }
+};
+
+const callbackXhrSuccess2 = (response) => {
+  console.log('callbackXhr2');
+  console.dir(response);
+
+  formTools.reset();
+  $('#goods-card').modal('hide');
+
+  switch (response.status) {
+  case 200:
+    catalogGroupsGoods.fill();
     break;
   case 400:
     markup.informationtModal = {
@@ -42,20 +71,33 @@ const callbackXhrSuccess = (response) => {
 
 const submitForm = () => {
   const stor = dataStorage.data;
-  const groupId = dataStorage.currentGroupId;
 
-  let postData = `token=${stor.token}&name=${name.value}&description=${describe.value}&purchase_price=${purchase.value}&selling_price=${sell.value}&group=${groupId}&barcode=${barcode.value}`;
-  let urlApp = appUrl.replace('{{dir}}', stor.directory);
+  let postData = `token=${stor.token}&name=${name.value}&description=${describe.value}&group=${groupId.value}&barcode=${barcode.value}`;
+  let urlApp = appUrl1.replace('{{dir}}', stor.directory);
   urlApp = urlApp.replace('{{oper}}', stor.operatorId);
   urlApp = urlApp.replace('{{busId}}', stor.currentBusiness);
+  urlApp = urlApp.replace('{{goodId}}', dataStorage.currentGoodId);
+
+  formTools.submit({
+    url: urlApp,
+    metod: 'PUT',
+    data: postData,
+    callbackSuccess: callbackXhrSuccess
+  });
+
+  postData = `token=${stor.token}&purchase_price=${purchase.value}&selling_price=${sell.value}`;
+  urlApp = appUrl2.replace('{{dir}}', stor.directory);
+  urlApp = urlApp.replace('{{oper}}', stor.operatorId);
+  urlApp = urlApp.replace('{{busId}}', stor.currentBusiness);
+  urlApp = urlApp.replace('{{goodId}}', dataStorage.currentGoodId);
 
   formTools.submit({
     url: urlApp,
     metod: 'POST',
     data: postData,
+    callbackSuccess: callbackXhrSuccess2
   });
 };
-
 
 const addHandlers = () => {
   $('#goods-card').on('hidden.bs.modal', () => {
@@ -63,7 +105,7 @@ const addHandlers = () => {
   });
 
   $('#goods-card').on('shown.bs.modal', () => {
-    formTools.work(form, submitForm, callbackXhrSuccess);
+    formTools.work(form, submitForm);
   });
 };
 
